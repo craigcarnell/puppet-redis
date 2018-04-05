25ffc907e4bac2f75d1c715beaf4f0793b3bd1f2
Craig Carnell <craig_carnell@hotmail.com>
2018-04-05 15:09:40 +0100

    Increase number of redis databases

--- manifests/params.pp
+++ manifests/params.pp
@@ -23,7 +23,7 @@ class redis::params {
   $redis_max_clients = false
   $redis_timeout = 300         # 0 = disabled
   $redis_loglevel = 'notice'
-  $redis_databases = 16
+  $redis_databases = 20
   $redis_slowlog_log_slower_than = 10000 # microseconds
   $redis_slowlog_max_len = 1024
   $redis_password = false
